640e1417dbff71f5058e299991cef370ed3b7773
Dairon M. Leyva <daironmichel@gmail.com>
2019-03-03 15:01:13 -0500

    added admin views and attributes to models (wip: process on upload)

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Supplier, Product, InventoryUpload
 
-# Register your models here.
+
+class InventoryUploadAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['supplier', 'file']}),
+        ('File Info', {'fields': ['price_col', 'identifier_col', 'identifier_type']}),
+    ]
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ('supplier__name', 'files', 'roi', 'sales_rank', 'profit', 'sold_by_amazon')
+
+
+admin.site.register(Supplier)
+admin.site.register(Product)
+admin.site.register(InventoryUpload, InventoryUploadAdmin)
--- products/models.py
+++ products/models.py
@@ -11,50 +11,89 @@ class TimeStamp(models.Model):
 
 class Supplier(TimeStamp):
     name = models.CharField(max_length=255)
-    website = models.CharField(max_length=255)
+    website = models.CharField(max_length=255, default='', blank=True)
+
+    def __str__(self):
+        return '<Supplier: {}, {}>'.format(self.id, self.name)
 
 
 class Product(TimeStamp):
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='products')
-    asin = models.CharField(max_length=15, verbose_name='ASIN')
-    upc = models.CharField(max_length=15, verbose_name='UPC')
-    ein = models.CharField(max_length=15, verbose_name='EIN')
-    sku = models.CharField(max_length=15, verbose_name='SKU')
-    wholesale_units = models.IntegerField(default=0)
-    wholesale_price = models.DecimalField(max_digits=11, decimal_places=2, default=0)
-    fees = models.DecimalField(max_digits=11, decimal_places=2, default=0)
-    total_cost = models.DecimalField(max_digits=11, decimal_places=2, default=0)
-    roi = models.DecimalField(max_digits=11, decimal_places=2, default=0, verbose_name='ROI')
-    buy_box = models.DecimalField(max_digits=11, decimal_places=2, default=0)
-    buy_box_avg90 = models.DecimalField(max_digits=11, decimal_places=2, default=0, verbose_name='Buy Box - 90 Days AVG')
-    buy_box_min = models.DecimalField(max_digits=11, decimal_places=2, default=0)
-    buy_box_max = models.DecimalField(max_digits=11, decimal_places=2, default=0)
-    buy_box_min_date = models.DateField()
-    buy_box_max_date = models.DateField()
-    review_count = models.IntegerField(default=0, verbose_name='Reviews')
-    review_count_last30 = models.IntegerField(default=0, verbose_name='Reviews - Last 30 Days')
-    review_count_avg90 = models.IntegerField(default=0, verbose_name='Reviews - 90 Days AVG')
-    sales_rank = models.IntegerField(default=0)
-    sales_rank_avg90 = models.IntegerField(default=0, verbose_name='Sales Rank - 90 Days AVG')
-    fba_sellers_count = models.IntegerField(default=0)
-    root_category = models.CharField(max_length=255)
-    sales_estimate = models.IntegerField(default=0)
-    three_month_supply_cost = models.DecimalField(max_digits=11, decimal_places=2, default=0)
-    three_month_supply_amount = models.IntegerField(default=0)
+    asin = models.CharField(max_length=15, verbose_name='ASIN', default='', blank=True)
+    upc = models.CharField(max_length=15, verbose_name='UPC', default='', blank=True)
+    ein = models.CharField(max_length=15, verbose_name='EIN', default='', blank=True)
+    sku = models.CharField(max_length=15, verbose_name='SKU', default='', blank=True)
+    name = models.CharField(max_length=255, default='', blank=True)
+    wholesale_units = models.IntegerField(default=0, blank=True)
+    wholesale_price = models.DecimalField(max_digits=11, decimal_places=2, default=0, blank=True)
+    fees = models.DecimalField(max_digits=11, decimal_places=2, default=0, blank=True)
+    total_cost = models.DecimalField(max_digits=11, decimal_places=2, default=0, blank=True)
+    roi = models.DecimalField(max_digits=11, decimal_places=2, default=0, blank=True, verbose_name='ROI')
+    buy_box = models.DecimalField(max_digits=11, decimal_places=2, default=0, blank=True)
+    buy_box_avg90 = models.DecimalField(max_digits=11, decimal_places=2, default=0, blank=True, verbose_name='Buy Box - 90 Days AVG')
+    buy_box_min = models.DecimalField(max_digits=11, decimal_places=2, default=0, blank=True)
+    buy_box_max = models.DecimalField(max_digits=11, decimal_places=2, default=0, blank=True)
+    buy_box_min_date = models.DateField(null=True, blank=True)
+    buy_box_max_date = models.DateField(null=True, blank=True)
+    review_count = models.IntegerField(default=0, blank=True, verbose_name='Reviews')
+    review_count_last30 = models.IntegerField(default=0, blank=True, verbose_name='Reviews - Last 30 Days')
+    review_count_avg90 = models.IntegerField(default=0, blank=True, verbose_name='Reviews - 90 Days AVG')
+    sales_rank = models.IntegerField(default=0, blank=True)
+    sales_rank_avg90 = models.IntegerField(default=0, blank=True, verbose_name='Sales Rank - 90 Days AVG')
+    fba_sellers_count = models.IntegerField(default=0, blank=True)
+    root_category = models.CharField(max_length=255, default='', blank=True)
+    sales_estimate = models.IntegerField(default=0, blank=True)
+    three_month_supply_cost = models.DecimalField(max_digits=11, decimal_places=2, default=0, blank=True)
+    three_month_supply_amount = models.IntegerField(default=0, blank=True)
+    sold_by_amazon = models.BooleanField(default=False)
 
-    PENDING, DISCARDED, HIGHLIGHTED, LIKED = range(4)
+    PENDING, DISCARDED, HIGHLIGHTED, LIKED, FAILED = range(5)
     STATUS_CHOICES = (
-        (PENDING, 'Pending'),  # New product on catalog, pending user review
-        (DISCARDED, 'Discarded'),  # Product doesn't sell well or user won't sell it for some reason
+        (PENDING, 'Pending Analysis'),  # New product on catalog, pending user review or system analysis
+        (DISCARDED, 'Thumbs Down :('),  # Product doesn't sell well or user won't sell it for some reason
         (HIGHLIGHTED, 'Highlighted'),  # good preliminary review but saved for later in-depth analysis
-        (LIKED, 'Approved'),  # good prospect
+        (LIKED, 'Thumbs Up :)'),  # good prospect
+        (FAILED, 'Analysis Failed'),  # System failed to analyse this product
     )
     status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING)
+    failed_analysis_reason = models.CharField(max_length=255, default='', blank=True)
+
+    def __str__(self):
+        return '<Product: {}, {}, {}>'.format(self.id, self.asin or 'NO_ASIN', self.name)
 
 
 class InventoryUpload(TimeStamp):
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='uploads')
     products = models.ManyToManyField(Product, related_name='files')
     file = models.FileField(upload_to='uploads/inventory/%Y/%m/%d')
+    price_col = models.CharField(max_length=255, default='Price', verbose_name='Price Column')
+    identifier_col = models.CharField(max_length=255, default='UPC', verbose_name='Identifier Column')
+    ASIN, UPC, EIN = range(3)
+    IDENTIFIER_CHOICES = (
+        (ASIN, 'ASIN'),
+        (UPC, 'UPC'),
+        (EIN, 'EIN'),
+    )
+    identifier_type = models.IntegerField(choices=IDENTIFIER_CHOICES, default=UPC)
+
+    PENDING, PROCESSING, FINISHED, FINISHED_WITH_ERRORS, FAILED = range(5)
+    STATUS_CHOICES = (
+        (PENDING, 'Analysis Pending'),
+        (PROCESSING, 'Analysis in Process'),
+        (FINISHED, 'Analysis Finished'),
+        (FINISHED_WITH_ERRORS, 'Analysis Finished With Some Failures'),
+        (FAILED, 'Analysis Failed'),
+    )
+    status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING)
+    failed_analysis_reason = models.CharField(max_length=255, default='', blank=True)
+
+    total_products = models.IntegerField(default=0, blank=True)
+    processed_products = models.IntegerField(default=0, blank=True)
+    analysed_products = models.IntegerField(default=0, blank=True)
+    failed_products = models.IntegerField(default=0, blank=True)
+    email_on_completion = models.BooleanField(default=True, blank=True)
+
+    def __str__(self):
+        return '<InventoryUpload: {}, {}, {}>'.format(self.id, self.file.filename, self.STATUS_CHOICES[self.status])
 
 
